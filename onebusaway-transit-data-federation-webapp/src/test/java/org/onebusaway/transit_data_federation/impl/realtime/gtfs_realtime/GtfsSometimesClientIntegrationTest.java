ca6f72770d78e6607dcb2819c385192ebbd5ff3b
Simon Jacobs <sidaja@gmail.com>
2018-08-14 15:56:34 -0400

    Testing in progress

---
+++ onebusaway-transit-data-federation-webapp/src/test/java/org/onebusaway/transit_data_federation/impl/realtime/gtfs_realtime/GtfsSometimesClientIntegrationTest.java
@@ -0,0 +1,151 @@
+/**
+ * Copyright (C) 2018 Cambridge Systematics, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onebusaway.transit_data_federation.impl.realtime.gtfs_realtime;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.onebusaway.gtfs.impl.GtfsRelationalDaoImpl;
+import org.onebusaway.gtfs.impl.calendar.CalendarServiceDataFactoryImpl;
+import org.onebusaway.gtfs.model.Agency;
+import org.onebusaway.gtfs.model.Stop;
+import org.onebusaway.gtfs.model.StopTime;
+import org.onebusaway.gtfs.model.Trip;
+import org.onebusaway.gtfs.model.calendar.CalendarServiceData;
+import org.onebusaway.gtfs.model.calendar.LocalizedServiceId;
+import org.onebusaway.gtfs.serialization.GtfsReader;
+import org.onebusaway.gtfs.services.calendar.CalendarServiceDataFactory;
+import org.onebusaway.transit_data.model.ListBean;
+import org.onebusaway.transit_data.model.trips.TripDetailsBean;
+import org.onebusaway.transit_data.model.trips.TripDetailsQueryBean;
+import org.onebusaway.transit_data.services.TransitDataService;
+import org.onebusaway.transit_data_federation.impl.transit_graph.AgencyEntryImpl;
+import org.onebusaway.transit_data_federation.impl.transit_graph.BlockConfigurationEntryImpl;
+import org.onebusaway.transit_data_federation.impl.transit_graph.BlockEntryImpl;
+import org.onebusaway.transit_data_federation.impl.transit_graph.RouteEntryImpl;
+import org.onebusaway.transit_data_federation.impl.transit_graph.StopEntryImpl;
+import org.onebusaway.transit_data_federation.impl.transit_graph.TripEntryImpl;
+import org.onebusaway.transit_data_federation.services.transit_graph.BlockConfigurationEntry;
+import org.onebusaway.transit_data_federation.services.transit_graph.BlockEntry;
+import org.onebusaway.transit_data_federation.services.transit_graph.ServiceIdActivation;
+import org.onebusaway.transit_data_federation.services.transit_graph.StopTimeEntry;
+import org.onebusaway.transit_data_federation.services.transit_graph.TransitGraphDao;
+import org.onebusaway.transit_data_federation.testing.UnitTestingSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration()
+@ContextConfiguration(locations = "classpath:org/onebusaway/transit_data_federation/tds-test.xml")
+@TestPropertySource(properties = { "bundlePath = /tmp/foo"})
+public class GtfsSometimesClientIntegrationTest {
+
+    @Autowired
+    private TransitDataService _tds;
+
+    @Autowired
+    private TransitGraphDao _graph;
+
+    private static final Logger _log = LoggerFactory.getLogger(GtfsSometimesClientIntegrationTest.class);
+
+    @Before
+    public void loadGtfsData() throws IOException {
+        GtfsReader reader = new GtfsReader();
+        String path = getClass().getResource("/gtfs_sometimes/gtfs_staten_island_S86.zip").getPath();
+        reader.setDefaultAgencyId("MTA");
+        reader.setInputLocation(new File(path));
+        GtfsRelationalDaoImpl dao = new GtfsRelationalDaoImpl();
+        reader.setEntityStore(dao);
+        reader.run();
+
+        for (Agency agency : dao.getAllAgencies()) {
+            AgencyEntryImpl aei = new AgencyEntryImpl();
+            aei.setId(agency.getId());
+            assertTrue(_graph.addAgencyEntry(aei));
+        }
+
+        Map<String, StopEntryImpl> stopById = new HashMap<>();
+
+        for (Stop stop : dao.getAllStops()) {
+            StopEntryImpl sei = new StopEntryImpl(stop.getId(), stop.getLat(), stop.getLon());
+            assertTrue(_graph.addStopEntry(sei));
+            stopById.put(stop.getId().getId(), sei);
+        }
+
+        CalendarServiceDataFactory csdf = new CalendarServiceDataFactoryImpl(dao);
+        CalendarServiceData csd = csdf.createData();
+        _graph.updateCalendarServiceData(csd);
+
+        for (Trip trip : dao.getAllTrips()) {
+            LocalizedServiceId lsi = new LocalizedServiceId(trip.getServiceId(), csd.getTimeZoneForAgencyId(trip.getId().getAgencyId()));
+            TripEntryImpl tei = new TripEntryImpl();
+            tei.setId(trip.getId());
+            tei.setServiceId(lsi);
+            BlockEntryImpl bei = new BlockEntryImpl();
+            bei.setId(trip.getId());
+            bei.setConfigurations(new ArrayList<>());
+            BlockConfigurationEntryImpl.Builder builder = BlockConfigurationEntryImpl.builder();
+
+            tei.setBlock(bei);
+            RouteEntryImpl rei = new RouteEntryImpl();
+            rei.setId(trip.getRoute().getId());
+            tei.setRoute(rei);
+            List<StopTimeEntry> stopTimes = new ArrayList<>();
+            for (StopTime stopTime : dao.getAllStopTimes()) {
+                String stopId = stopTime.getStop().getId().getId();
+                stopTimes.add(UnitTestingSupport.stopTime(stopTime.getId(), stopById.get(stopId), tei,
+                        stopTime.getArrivalTime(), stopTime.getDepartureTime(), (int) Math.round(stopTime.getShapeDistTraveled())));
+            }
+            tei.setStopTimes(stopTimes);
+            builder.setBlock(bei);
+            builder.setTrips(Collections.singletonList(tei));
+            builder.setTripGapDistances(new double[] { 0 });
+            builder.setServiceIds(new ServiceIdActivation(lsi));
+            bei.getConfigurations().add(builder.create());
+
+            assertTrue(_graph.addTripEntry(tei));
+        }
+    }
+
+    @Test
+    public void testLoadSuccess() {
+        String tripId = "CA_G8-Weekday-096000_MISC_545";
+        TripDetailsQueryBean query = new TripDetailsQueryBean();
+        query.setTripId("MTA_" + tripId);
+        query.setServiceDate(UnitTestingSupport.dateAsLong("2018-08-14 00:00")/1000);
+        ListBean<TripDetailsBean> tripDetailsList = _tds.getTripDetails(query);
+        assertEquals(1, tripDetailsList.getList().size());
+        TripDetailsBean tripDetails = tripDetailsList.getList().get(0);
+        assertEquals(71, tripDetails.getSchedule().getStopTimes().size());
+    }
+
+}
