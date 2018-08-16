2e518e9b45cf4a2d3c188991e6a4da34773b31c4
Simon Jacobs <sidaja@gmail.com>
2018-08-16 11:51:29 -0400

    Some work on GTFS-Sometimes client integration test. Refactored some testing support methods. GTFS-Sometimes Handler now uses GtfsRealtimeEntitySource, borrowed from RT handling code.

---
+++ onebusaway-transit-data-federation/src/main/java/org/onebusaway/transit_data_federation/testing/ServiceChangeUnitTestingSupport.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (C) 2018 Cambridge Systematics, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onebusaway.transit_data_federation.testing;
+
+import com.camsys.transit.servicechange.DateDescriptor;
+import com.camsys.transit.servicechange.EntityDescriptor;
+import com.camsys.transit.servicechange.ServiceChange;
+import com.camsys.transit.servicechange.ServiceChangeType;
+import com.camsys.transit.servicechange.Table;
+import com.camsys.transit.servicechange.field_descriptors.AbstractFieldDescriptor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServiceChangeUnitTestingSupport {
+    public static EntityDescriptor stopTimeEntity(String tripId, String stopId) {
+        EntityDescriptor desc = new EntityDescriptor();
+        desc.setTripId(tripId);
+        desc.setStopId(stopId);
+        return desc;
+    }
+
+    public static List<DateDescriptor> dateDescriptors(LocalDate... dates) {
+        List<DateDescriptor> descriptors = new ArrayList<>();
+        for (LocalDate ld : dates) {
+            DateDescriptor d = new DateDescriptor();
+            d.setDate(ld);
+            descriptors.add(d);
+        }
+        return descriptors;
+    }
+
+    public static List<DateDescriptor> dateDescriptorsRange(LocalDate... dates) {
+        List<DateDescriptor> descriptors = new ArrayList<>();
+        for (int i = 0; i < dates.length/2; i++) {
+            LocalDate from = dates[i];
+            LocalDate to = dates[i+1];
+            DateDescriptor desc = new DateDescriptor();
+            desc.setFrom(from);
+            desc.setTo(to);
+            descriptors.add(desc);
+        }
+        return descriptors;
+    }
+
+    public static ServiceChange serviceChange(Table table, ServiceChangeType type, List<EntityDescriptor> entities,
+                                               List<AbstractFieldDescriptor> descriptors, List<DateDescriptor> dates) {
+        ServiceChange change = new ServiceChange();
+        change.setTable(table);
+        change.setServiceChangeType(type);
+        change.setAffectedEntity(entities);
+        change.setAffectedField(descriptors);
+        change.setAffectedDates(dates);
+        return change;
+    }
+}
--- onebusaway-transit-data-federation/src/test/java/org/onebusaway/transit_data_federation/impl/realtime/gtfs_sometimes/GtfsSometimesHandlerImplTest.java
+++ onebusaway-transit-data-federation/src/test/java/org/onebusaway/transit_data_federation/impl/realtime/gtfs_sometimes/GtfsSometimesHandlerImplTest.java
@@ -15,26 +15,23 @@
  */
 package org.onebusaway.transit_data_federation.impl.realtime.gtfs_sometimes;
 
-import com.camsys.transit.servicechange.DateDescriptor;
-import com.camsys.transit.servicechange.EntityDescriptor;
 import com.camsys.transit.servicechange.ServiceChange;
 import com.camsys.transit.servicechange.ServiceChangeType;
 import com.camsys.transit.servicechange.Table;
-import com.camsys.transit.servicechange.field_descriptors.AbstractFieldDescriptor;
 import org.junit.Before;
 import org.junit.Test;
 import org.onebusaway.gtfs.model.AgencyAndId;
+import org.onebusaway.transit_data_federation.impl.realtime.gtfs_realtime.GtfsRealtimeEntitySource;
 import org.onebusaway.transit_data_federation.services.transit_graph.TransitGraphDao;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collections;
-import java.util.List;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+import static org.onebusaway.transit_data_federation.testing.ServiceChangeUnitTestingSupport.*;
 
 public class GtfsSometimesHandlerImplTest {
 
@@ -45,12 +42,15 @@ public class GtfsSometimesHandlerImplTest {
     @Before
     public void setup() {
         dao = mock(TransitGraphDao.class);
+        when(dao.deleteStopTime(any(), any())).thenReturn(true);
         handler = new GtfsSometimesHandlerImpl();
         handler.setTransitGraphDao(dao);
         handler.setAgencyId("1");
         Calendar cal = Calendar.getInstance();
         cal.set(2018, Calendar.JULY, 1, 13, 0, 0);
         handler.setTime(cal.getTimeInMillis());
+        GtfsRealtimeEntitySource source = new MockEntitySource() { };
+        handler.setEntitySource(source);
     }
 
     @Test
@@ -132,45 +132,15 @@ public class GtfsSometimesHandlerImplTest {
                 AgencyAndId.convertFromString("1_stopA"));
     }
 
-    private static EntityDescriptor stopTimeEntity(String tripId, String stopId) {
-        EntityDescriptor desc = new EntityDescriptor();
-        desc.setTripId(tripId);
-        desc.setStopId(stopId);
-        return desc;
-    }
-
-    private static List<DateDescriptor> dateDescriptors(LocalDate... dates) {
-        List<DateDescriptor> descriptors = new ArrayList<>();
-        for (LocalDate ld : dates) {
-            DateDescriptor d = new DateDescriptor();
-            d.setDate(ld);
-            descriptors.add(d);
+    private abstract class MockEntitySource extends GtfsRealtimeEntitySource {
+        @Override
+        public AgencyAndId getObaTripId(String tripId) {
+            return new AgencyAndId("1", tripId);
         }
-        return descriptors;
-    }
 
-    private static List<DateDescriptor> dateDescriptorsRange(LocalDate... dates) {
-        List<DateDescriptor> descriptors = new ArrayList<>();
-        for (int i = 0; i < dates.length/2; i++) {
-            LocalDate from = dates[i];
-            LocalDate to = dates[i+1];
-            DateDescriptor desc = new DateDescriptor();
-            desc.setFrom(from);
-            desc.setTo(to);
-            descriptors.add(desc);
+        @Override
+        public AgencyAndId getObaStopId(String stopId) {
+            return new AgencyAndId("1", stopId);
         }
-        return descriptors;
     }
-
-    private static ServiceChange serviceChange(Table table, ServiceChangeType type, List<EntityDescriptor> entities,
-                                               List<AbstractFieldDescriptor> descriptors, List<DateDescriptor> dates) {
-        ServiceChange change = new ServiceChange();
-        change.setTable(table);
-        change.setServiceChangeType(type);
-        change.setAffectedEntity(entities);
-        change.setAffectedField(descriptors);
-        change.setAffectedDates(dates);
-        return change;
-    }
-
 }
