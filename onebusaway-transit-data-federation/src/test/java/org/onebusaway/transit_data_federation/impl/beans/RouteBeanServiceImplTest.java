camsys/onebusaway-application-modules

commit 2b4fde5da259ab1080551a53aa2ee1e06cf1d1a7

GTFS-242 fix issue with modified stops appearing twice in route list

onebusaway-transit-data-federation-webapp/src/test/java/org/onebusaway/transit_data_federation/impl/realtime/gtfs_sometimes/impl/GtfsSometimesClientIntegrationTest.java
@@ -560,6 +560,9 @@ public class GtfsSometimesClientIntegrationTest {
         double oldLat = stopBean.getLat();
         double oldLon = stopBean.getLon();
 
+        StopsForRouteBean stopsForRoute = _tds.getStopsForRoute("MTA NYCT_S86");
+        List<String> stopGroupStops = stopsForRoute.getStopGroupings().get(0).getStopGroups().get(0).getStopIds();
+
         double newLat = 40.557318, newLon = -74.1141876;
         ServiceChange change = serviceChange(Table.STOPS,
                 ServiceChangeType.ALTER,
@@ -578,6 +581,10 @@ public class GtfsSometimesClientIntegrationTest {
         TripStopTimeBean stop = tripDetails.getSchedule().getStopTimes().get(67);
         assertEquals(newLat, stop.getStop().getLat(), 0.00001);
         assertEquals(newLon, stop.getStop().getLon(), 0.00001);
+
+        // List of stops should stay the same.
+        stopsForRoute = _tds.getStopsForRoute("MTA NYCT_S86");
+        List<String> stopGroupStopsAltered = stopsForRoute.getStopGroupings().get(0).getStopGroups().get(0).getStopIds();
 
         revertPreviousChanges();
         stopBean = _tds.getStop("MTA_203564");
@@ -605,6 +612,11 @@ public class GtfsSometimesClientIntegrationTest {
             assertEquals(oldStopTime.getGtfsSequence(), revertStopTime.getGtfsSequence());
             assertEquals(oldStopTime.getDistanceAlongTrip(), revertStopTime.getDistanceAlongTrip(), 0.00001);
         }
+
+        stopsForRoute = _tds.getStopsForRoute("MTA NYCT_S86");
+        List<String> stopGroupStopsFinal = stopsForRoute.getStopGroupings().get(0).getStopGroups().get(0).getStopIds();
+        assertEquals(stopGroupStops, stopGroupStopsAltered);
+        assertEquals(stopGroupStops, stopGroupStopsFinal);
     }
 
     // Test whether location change is successfully reverted and new change is applied

onebusaway-transit-data-federation/src/main/java/org/onebusaway/transit_data_federation/impl/beans/RouteBeanServiceImpl.java
@@ -284,6 +284,9 @@ class RouteBeanServiceImpl implements RouteBeanService {
       StopEntry prev = null;
       for (StopEntry stop : sequence.getStops()) {
         if (prev != null) {
+          // Normalize stop in case there are service changes
+          stop = _transitGraphDao.getStopEntryForId(stop.getId());
+
           // We do this to avoid cycles
           if (!graph.isConnected(stop, prev))
             graph.addEdge(prev, stop);

commit 9424e5391ed175544ff777e838c09d5d55462b0f

Fix bug introduced in 2b4fde5da

onebusaway-transit-data-federation/src/test/java/org/onebusaway/transit_data_federation/impl/beans/RouteBeanServiceImplTest.java
@@ -18,6 +18,7 @@ package org.onebusaway.transit_data_federation.impl.beans;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
+import static org.onebusaway.transit_data_federation.testing.UnitTestingSupport.aid;
 import static org.onebusaway.transit_data_federation.testing.UnitTestingSupport.block;
 import static org.onebusaway.transit_data_federation.testing.UnitTestingSupport.blockTripIndices;
 import static org.onebusaway.transit_data_federation.testing.UnitTestingSupport.linkBlockTrips;
@@ -177,6 +178,10 @@ public class RouteBeanServiceImplTest {
     StopEntryImpl stopB = stop("stopB", 47.1, -122.1);
     StopEntryImpl stopC = stop("stopC", 47.2, -122.2);
 
+    Mockito.when(_transitGraphDao.getStopEntryForId(aid("stopA"))).thenReturn(stopA);
+    Mockito.when(_transitGraphDao.getStopEntryForId(aid("stopB"))).thenReturn(stopB);
+    Mockito.when(_transitGraphDao.getStopEntryForId(aid("stopC"))).thenReturn(stopC);
+    
     BlockEntryImpl blockA = block("blockA");
     TripEntryImpl tripA = trip("tripA", "sidA");
     TripEntryImpl tripB = trip("tripB", "sidA");