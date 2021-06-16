8b0f4a05a9a390e31e783fb49a8e2467b960a5f2
carabalb <lennycaraballo@gmail.com>
2021-06-16 11:42:19 -0400

    Cleaning up CustomJsonLibHandler

--- onebusaway-api-webapp/src/main/java/org/onebusaway/api/serializers/json/CustomJsonLibHandler.java
+++ onebusaway-api-webapp/src/main/java/org/onebusaway/api/serializers/json/CustomJsonLibHandler.java
@@ -3,46 +3,112 @@ package org.onebusaway.api.serializers.json;
 import com.fasterxml.jackson.databind.*;
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.inject.Inject;
+import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.rest.handler.AbstractContentTypeHandler;
+import org.apache.struts2.rest.handler.ContentTypeHandler;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import net.sf.json.JsonConfig;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
+import java.util.Collection;
 
 
-public class CustomJsonLibHandler extends AbstractContentTypeHandler {
+public class CustomJsonLibHandler implements ContentTypeHandler {
 
-  private static final String DEFAULT_CONTENT_TYPE = "application/json";
-  private String defaultEncoding = "ISO-8859-1";
-  private ObjectMapper mapper = new ObjectMapper();
-
-
-  public void toObject(ActionInvocation invocation, Reader in, Object target) throws IOException {
-    this.mapper.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false);
-    ObjectReader or = this.mapper.readerForUpdating(target);
-    or.readValue(in);
+  @Deprecated
+  @Override
+  public void toObject(Reader in, Object target) throws IOException {
+    return;
   }
 
-  public String fromObject(ActionInvocation invocation, Object obj, String resultCode, Writer stream) throws IOException {
+  @Override
+  public void toObject(ActionInvocation actionInvocation, Reader in, Object target) throws IOException {
+    StringBuilder sb = new StringBuilder();
+    char[] buffer = new char[1024];
+    int len = 0;
+    while ((len = in.read(buffer)) > 0) {
+      sb.append(buffer, 0, len);
+    }
+    if (target != null && sb.length() > 0 && sb.charAt(0) == '[') {
+      JSONArray jsonArray = JSONArray.fromObject(sb.toString());
+      if (target.getClass().isArray()) {
+        JSONArray.toArray(jsonArray, target, new JsonConfig());
+      } else {
+        JSONArray.toList(jsonArray, target, new JsonConfig());
+      }
 
-    this.mapper.setSerializerProvider(new CustomSerializerProvider());
-    this.mapper.configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true);
-    this.mapper.configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true);
-    this.mapper.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false);
-    this.mapper.writeValue(stream, obj);
+    } else {
+      JSONObject jsonObject = JSONObject.fromObject(sb.toString());
+      JSONObject.toBean(jsonObject, target, new JsonConfig());
+    }
+  }
+
+
+  public String fromObject(Object obj, String resultCode, Writer stream, String callback)
+          throws IOException {
+
+    String value = null;
+
+    if (obj != null) {
+      if (isArray(obj)) {
+        JSONArray jsonArray = JSONArray.fromObject(obj);
+        value = jsonArray.toString();
+      } else {
+        JSONObject jsonObject = JSONObject.fromObject(obj);
+        value = jsonObject.toString();
+      }
+    }
+
+    if (value != null) {
+      if (callback != null)
+        stream.write(callback + "(" + value + ")");
+      else
+        stream.write(value);
+    }
+
     return null;
   }
 
+  @Deprecated
+  @Override
+  public String fromObject(Object obj, String resultCode, Writer stream)
+          throws IOException {
+    return null;
+
+  }
+
+  @Override
+  public String fromObject(ActionInvocation actionInvocation, Object obj, String resultCode, Writer stream) throws IOException {
+    String callback = getCallback();
+    return fromObject(obj, resultCode, stream, callback);
+  }
+
+  private boolean isArray(Object obj) {
+    return obj instanceof Collection<?> || obj.getClass().isArray();
+  }
+
   public String getContentType() {
-    return "application/json;charset=" + this.defaultEncoding;
+    String callback = getCallback();
+    if(callback != null){
+      return ("application/javascript");
+    }
+    return "application/json";
+  }
+
+  private String getCallback(){
+    String callback = null;
+    HttpServletRequest req = ServletActionContext.getRequest();
+    if (req != null) {
+      callback = req.getParameter("callback");
+    }
+    return callback;
   }
 
   public String getExtension() {
     return "json";
-  }
-
-  @Inject("struts.i18n.encoding")
-  public void setDefaultEncoding(String val) {
-    this.defaultEncoding = val;
   }
 }
