0de0f5717741d116fc7265163897f571d917d4b4
carabalb <lennycaraballo@gmail.com>
2021-07-20 18:22:43 -0400

    Using column length to define long blob instead of column definition
    This way column is compatible with both MySQL and hsqldb

--- onebusaway-users/src/main/java/org/onebusaway/users/model/User.java
+++ onebusaway-users/src/main/java/org/onebusaway/users/model/User.java
@@ -96,7 +96,7 @@ public class User extends IdentityBean<Integer> {
   private boolean temporary;
 
   @Lob
-  @Column(columnDefinition = "longblob")
+  @Column(length=16777216)
   private UserProperties properties;
 
   @ManyToMany(fetch = FetchType.EAGER)
