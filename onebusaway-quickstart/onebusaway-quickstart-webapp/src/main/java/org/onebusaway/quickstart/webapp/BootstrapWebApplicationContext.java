5f429198d7cde05e0193ab94fbd96e4d3135f4f4
rjasmin-camsys <rjasmin@camsys.com>
2024-08-30 14:20:08 -0400

    still does not compile, but made some changes to poms to fix some issues, jakarta upgrade

--- onebusaway-quickstart/onebusaway-quickstart-webapp/src/main/java/org/onebusaway/quickstart/webapp/BootstrapWebApplicationContext.java
+++ onebusaway-quickstart/onebusaway-quickstart-webapp/src/main/java/org/onebusaway/quickstart/webapp/BootstrapWebApplicationContext.java
@@ -20,7 +20,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
-import javax.servlet.ServletContext;
+import jakarta.servlet.ServletContext;
 
 import org.apache.commons.cli.CommandLine;
 import org.onebusaway.quickstart.WebappCommon;
