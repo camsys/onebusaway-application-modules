de401016945e5b59b43ff843bb977f825edf4b0e
msalvatore <msalvatore@camsys.com>
2026-02-03 11:20:18 -0500

    [OBANYC-4037] add shape refactor and add agency

--- onebusaway-transit-data-federation/src/main/java/org/onebusaway/transit_data_federation/impl/realtime/gtfs_tripmodifications/service/ShapeHandler.java
+++ onebusaway-transit-data-federation/src/main/java/org/onebusaway/transit_data_federation/impl/realtime/gtfs_tripmodifications/service/ShapeHandler.java
@@ -1,6 +1,7 @@
 package org.onebusaway.transit_data_federation.impl.realtime.gtfs_tripmodifications.service;
 
 import com.google.transit.realtime.GtfsRealtime.Shape;
+import org.onebusaway.transit_data_federation.model.ShapePoints;
 
 import java.util.List;
 
@@ -9,7 +10,7 @@ public interface ShapeHandler {
      * Add a set of shapes, returning the number of successfully added shapes.
      *
      * @param shapes to add
-     * @return number of successfully added stops
+     * @return list of successfully added shapepoints
      */
-    int addShapes(List<Shape> shapes);
+     List<ShapePoints> addShapes(List<Shape> shapes);
 }
